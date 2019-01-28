7276b202c6594e02c69d64b4168cb4481911d24a
Luke Beatty <lukebeatty@Lukes-MBP.fc.ca>
2019-01-28 15:13:06 -0500

    adding index and Js (the JS is adding querySelectors for clicking on img and change text in the page)

--- script/imageAnim.js
+++ script/imageAnim.js
@@ -1,4 +1,16 @@
 (() => {
+	// Seclet the elements that you want to work with
+	let theButton = document.querySelector('#buttonHolder img');
+	// window.addEventListener("load", changeHeaderText);
+
+	function changeHeaderText() {
+		document.querySelector("h1").textContent = "Hello Lucas"
+		document.querySelector("p").textContent = "lets play a game my dude"
+
+	}
+	//  Event handling goes at the bottom
+	theButton.addEventListener("click", changeHeaderText);
 	// set up the puzzle pieces and boards
+
 	
 })();
