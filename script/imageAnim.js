a4bf437bfa1aa826d477f7f589cf47bddfdafa75
Luke Beatty <lukebeatty@Lukes-MBP.fc.ca>
2019-01-28 16:24:58 -0500

    made the puzzle work with clicks (still not click and drag) but swaps when clicked on

--- script/imageAnim.js
+++ script/imageAnim.js
@@ -1,16 +1,33 @@
 (() => {
-	// Seclet the elements that you want to work with
-	let theButton = document.querySelector('#buttonHolder img');
-	// window.addEventListener("load", changeHeaderText);
+	// var always first
+	// set up the Puzzle pieces and boards
+	const pieces = ["topLeft", "topRight", "bottomLeft", "bottomRight"];
 
-	function changeHeaderText() {
-		document.querySelector("h1").textContent = "Hello Lucas"
-		document.querySelector("p").textContent = "lets play a game my dude"
+	let piecesBoard = document.querySelector(".puzzle-pieces"),
+		puzzleBoard = document.querySelector(".puzzle-board"),
+		puzzleSelectors = document.querySelectorAll("#buttonHolder img");
 
+	// functions go in the middle
+	function creatPuzzlePieces(pictureIndex) {
+		// genertate puzzle pieces for the left hand side
+		pieces.forEach((piece, index) => {
+			let newPuzzlePiece = `<img "id=piece${index}" class="puzzle-image" src="images/${piece + pictureIndex}.jpg" alt="thumbnail">`;
+		
+			piecesBoard.innerHTML += newPuzzlePiece;
+		});
+
+		puzzleBoard.style.backgroundImage = `url(./images/backGround${pictureIndex}.jpg)`;
 	}
-	//  Event handling goes at the bottom
-	theButton.addEventListener("click", changeHeaderText);
-	// set up the puzzle pieces and boards
 
+	function resetPuzzlePieces() {
+		// empty the thumbnail container
+		piecesBoard.innerHTML = "";
+		creatPuzzlePieces(this.dataset.puzzleref)
+	}
+
+	// event handling down here
+	puzzleSelectors.forEach(puzzle => puzzle.addEventListener("click", resetPuzzlePieces));
+
+	creatPuzzlePieces(0);
 	
 })();
